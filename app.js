94358cd8e7d0602e689e5811050b4cefcb033084
Андрей Железняков <zhelezny.andrey@gmail.com>
2021-06-13 19:11:31 +0300

    move code to new class clockArrow

--- app.js
+++ app.js
@@ -2,7 +2,77 @@
 // x = a+r*cost
 // y = a+r*sint
 
-class clockArrow {}
+class clockArrow {
+  //#coordX = 0;
+  //#coordY;
+  //#radius;
+  // #context;
+  //#fi = 270;
+
+  constructor(x, y, rad, canvasContext) {
+    this._coordX = x;
+    this._coordY = y;
+    this._radius = rad;
+    this._context = canvasContext;
+    this._fi = 270;
+
+    setInterval(() => {
+      this.secondTick();
+    }, 1000);
+  }
+
+  secondTick() {
+    let x = this.calcX();
+    let y = this.calcY();
+    this.drawSeconds(x, y, "blue");
+    this._fi += 6;
+  }
+
+  drawSeconds(x, y, color) {
+    let SavedRect = this.rectCalc(this._coordX, this._coordY, x, y);
+    let SavedImage = this._context.getImageData(
+      SavedRect[0],
+      SavedRect[1],
+      SavedRect[2],
+      SavedRect[3]
+    );
+
+    this._context.strokeStyle = color;
+    this._context.beginPath();
+    this._context.moveTo(this._coordX, this._coordY);
+    this._context.lineTo(x, y);
+    this._context.stroke();
+
+    setTimeout(() => {
+      this._context.putImageData(SavedImage, SavedRect[0], SavedRect[1]);
+    }, 990);
+  }
+  calcX() {
+    let cosT = Math.cos((Math.PI * this._fi) / 180);
+    return Math.round(this._coordX + this._radius * cosT);
+  }
+  calcY() {
+    let sinT = Math.sin((Math.PI * this._fi) / 180);
+    return Math.round(this._coordX + this._radius * sinT);
+  }
+
+  rectCalc(x0, y0, x, y) {
+    let mas = [];
+    mas[0] = x0 < x ? x0 : x;
+    mas[0]--;
+    mas[1] = y0 < y ? y0 : y;
+    mas[1]--;
+
+    mas[2] = x0 > x ? x0 : x;
+    mas[2]++;
+    mas[3] = y0 > y ? y0 : y;
+    mas[3]++;
+
+    //console.log(mas);
+
+    return mas;
+  }
+}
 
 let el = document.getElementById("board");
 let ctx = el.getContext("2d");
@@ -12,60 +82,6 @@ ctx.beginPath();
 ctx.arc(100, 100, 105, 0, 2 * Math.PI);
 ctx.stroke();
 
-setInterval(function () {
-  secondTick();
-}, 1000);
+const ar = new clockArrow(100, 100, 100, ctx);
 
-function secondTick() {
-  let x = calcX(fi, 100);
-  let y = calcY(fi, 100);
-  drawSeconds(x, y, "blue");
-  fi += 6;
-}
-
-function calcX(t, rad) {
-  let cosT = Math.cos((Math.PI * t) / 180);
-  return Math.round(100 + rad * cosT);
-  //return Math.round(Math.cos((2 * Math.PI * index) / 60) * rad + 100);
-}
-function calcY(t, rad) {
-  let sinT = Math.sin((Math.PI * t) / 180);
-  return Math.round(100 + rad * sinT);
-  //return Math.round(Math.sin((2 * Math.PI * index) / 60) * rad + 100);
-}
-function drawSeconds(x, y, color) {
-  let SavedRect = rectCalc(100, 100, x, y);
-  let SavedImage = ctx.getImageData(
-    SavedRect[0],
-    SavedRect[1],
-    SavedRect[2],
-    SavedRect[3]
-  );
-
-  ctx.strokeStyle = color;
-  ctx.beginPath();
-  ctx.moveTo(100, 100);
-  ctx.lineTo(x, y);
-  ctx.stroke();
-
-  setTimeout(function () {
-    ctx.putImageData(SavedImage, SavedRect[0], SavedRect[1]);
-  }, 990);
-}
-
-function rectCalc(x0, y0, x, y) {
-  let mas = [];
-  mas[0] = x0 < x ? x0 : x;
-  mas[0]--;
-  mas[1] = y0 < y ? y0 : y;
-  mas[1]--;
-
-  mas[2] = x0 > x ? x0 : x;
-  mas[2]++;
-  mas[3] = y0 > y ? y0 : y;
-  mas[3]++;
-
-  console.log(mas);
-
-  return mas;
-}
+const ar2 = new clockArrow(200, 200, 50, ctx);
